5ffa87e2f72ab086d28a3811b1b6eca2a480edf6
Wililee <leewks@gmail.com>
2023-07-18 14:15:41 -0400

    added more photos

--- pages/projects.js
+++ pages/projects.js
@@ -5,11 +5,13 @@ import {HorizontalCardContainer} from '../public/components/containers/Horizonta
 import {BasePage} from '../public/components/containers/BasePage'
 import {ProjectCard} from '../public/components/containers/ProjectCard'
 import {AchievementCard} from '../public/components/containers/AchievementCard'
-import { AcademicCapIcon, TrophyIcon, WrenchIcon  } from "@heroicons/react/24/outline";
+import { AcademicCapIcon, TrophyIcon, WrenchIcon, HandRaisedIcon, NoSymbolIcon  } from "@heroicons/react/24/outline";
 
 const graduationIcon = <AcademicCapIcon className="h-8 w-8" />
 const trophyIcon = <TrophyIcon className="h-8 w-8" />
 const wrenchIcon = <WrenchIcon className="h-8 w-8" />
+const cancelIcon = <NoSymbolIcon  className="h-8 w-8" />
+const handIcon = <HandRaisedIcon className="h-8 w-8" />
 
 export default function projects() {
   return (
@@ -20,18 +22,18 @@ export default function projects() {
             <ProjectCard title="SoBar" description="SoBar is a bar hopping app used to create an optimal path for hoping inbetween bars on a night out." skills={["Java", "SQL"]} gitURL="https://github.com/Wililee/SoBarApp"/>
             <ProjectCard title="Project Olympian" description="Health and fitness app with a focus on Social Media and workout sharing." skills={["TypeScript", "React Native", "SQL"]} gitURL="https://github.com/dimitritsampiras/olympian-mobile-app"/>
         </HorizontalCardContainer>
-        <h1 className='mb-4 text-4xl leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl pl-8 pt-8'>Achievements</h1>
+        <h1 className='mb-4 text-4xl leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl pl-8 pt-8'>Achievements and fun</h1>
         <HorizontalCardContainer>
             <AchievementCard title="Graduation" description="I completed my degree in Software Engineering May 2023!" icon={graduationIcon} />
             <AchievementCard title="My Website" description="This very website!" />
             <AchievementCard title="My Website" description="This very website!" />
-            <AchievementCard title="My Website" description="This very website!" />
+            <AchievementCard title="Am I cancelled?" description="A twitter webscraper used to determine if someone is 'cancelled' " icon={cancelIcon} URL={"https://devpost.com/software/am-i-cancelled"}/>
         </HorizontalCardContainer>
         <HorizontalCardContainer>
             <AchievementCard title="Deltahacks" description="A winning Healthcare Assessment application" icon={trophyIcon} URL={"https://devpost.com/software/nursing-handbook-pro"}/>
             <AchievementCard title="Spacehacks" description="The popular choice award for our shiftworker scheduling app" icon={trophyIcon} />
             <AchievementCard title="Shell Eco Marathon" description="A competition to create a super-eco car" icon={wrenchIcon}/>
-            <AchievementCard title="My Website" description="This very website!" />
+            <AchievementCard title="Sign Language Reader" description="A machine learning model used to read basic sign language" icon={handIcon} URL={"https://devpost.com/software/asl-translator-fewj0t"}/>
         </HorizontalCardContainer>
     </BasePage>
   )
