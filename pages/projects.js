fe25d2290ea0f28ff8f30148e36fa2e9ebd8a6c6
Wililee <leewks@gmail.com>
2023-08-01 13:14:06 -0400

    corrected spelling errors oops

--- pages/projects.js
+++ pages/projects.js
@@ -21,7 +21,7 @@ export default function projects() {
         <h1 className='mb-4 text-4xl leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl pl-8 pt-8'>Featured Projects</h1>
         <HorizontalCardContainer className='pl-20'>
             <ProjectCard title="My Website" description="This very website!" skills={["JavaScript", "React", "CSS"]} gitURL="https://github.com/Wililee/myWebite"/>
-            <ProjectCard title="SoBar" description="SoBar is a bar hopping app used to create an optimal path for hoping inbetween bars on a night out." skills={["Java", "SQL"]} gitURL="https://github.com/Wililee/SoBarApp"/>
+            <ProjectCard title="SoBar" description="SoBar is a bar hopping app used to create an optimal path for walking inbetween bars on a night out." skills={["Java", "SQL"]} gitURL="https://github.com/Wililee/SoBarApp"/>
             <ProjectCard title="Project Olympian" description="Health and fitness app with a focus on Social Media and workout sharing." skills={["TypeScript", "React Native", "SQL"]} gitURL="https://github.com/dimitritsampiras/olympian-mobile-app"/>
         </HorizontalCardContainer>
         <h1 className='mb-4 text-4xl leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl pl-8 pt-8'>Achievements and fun</h1>
@@ -29,7 +29,7 @@ export default function projects() {
             <AchievementCard title="Graduation" description="I completed my degree in Software Engineering May 2023!" icon={graduationIcon} />
             <AchievementCard title="Am I cancelled?" description="A twitter webscraper used to determine if someone is 'cancelled' " icon={cancelIcon} URL={"https://devpost.com/software/am-i-cancelled"}/>
             <AchievementCard title="Deltahacks" description="A winning Healthcare Assessment application" icon={trophyIcon} URL={"https://devpost.com/software/nursing-handbook-pro"}/>
-            <AchievementCard title="Student Houze" description="A neet way to manage your student living" icon={homeIcon} URL={"https://devpost.com/software/student-house-manager"}/>
+            <AchievementCard title="Student Houze" description="A neat way to manage your student living" icon={homeIcon} URL={"https://devpost.com/software/student-house-manager"}/>
         </HorizontalCardContainer>
         <HorizontalCardContainer>
             <AchievementCard title="Sign Language Reader" description="A machine learning model used to read basic sign language" icon={handIcon} URL={"https://devpost.com/software/asl-translator-fewj0t"}/>
